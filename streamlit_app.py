41e46b4987d8e959628fde094d19c16afd0bef30
Maaz Ur Rehman <75684203+Maaz-Ur-Rehman@users.noreply.github.com>
2025-08-13 12:15:20 +0500

    Update streamlit_app.py
    add some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,11 @@ if ingredients_list and name_on_order.strip():
 elif st.button('Submit Order'):
     st.error("Please select at least one ingredient and enter a name for your smoothie.")
 
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        # st.text(smoothiefroot_response.json())
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
