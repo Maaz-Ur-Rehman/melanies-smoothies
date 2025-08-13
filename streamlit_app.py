2ed49fc5ae3998ad9ff50ebff87affbf4e8bd9f8
Maaz Ur Rehman <75684203+Maaz-Ur-Rehman@users.noreply.github.com>
2025-08-13 11:59:48 +0500

    Update streamlit_app.py
    add json

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ elif st.button('Submit Order'):
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
